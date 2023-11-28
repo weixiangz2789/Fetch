105d4691042b1502833b5f4e1dbd6037439f065c
1E10 Laptop <sample@bths.edu>
2023-11-28 11:02:56 -0500

    w

---
+++ fetch.js
@@ -0,0 +1,20 @@
+loopArr()
+  .then((response) => {
+    console.log("Success");
+  })
+  .catch((error) => {
+    console.log("Error");
+  });
+
+async function displayImage(x) {
+  console.log(`img/${x}.jfif`);
+  const response = await fetch(`img/${x}.jfif`);
+  const blob = await response.blob();
+  document.getElementById(x).src = URL.createObjectURL(blob);
+}
+
+const imgArr = ["tea", "lemon", "frenchie", "pasta"];
+
+function loopArr() {
+  imgArr.forEach((x) => console.log(x));
+}
